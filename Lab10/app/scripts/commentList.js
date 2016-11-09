8bacf34bf9438cdd8e002aae6fef81680d85d8d1
Loganvp <loganvp@gmail.com>
2016-11-08 21:05:42 -0500

    lab10 begin

---
+++ Lab10/app/scripts/commentList.js
@@ -0,0 +1,25 @@
+//This was split off because of Exercise 9.2
+
+//external/local imports required by this module…
+import React from 'react';
+import $ from 'jquery';
+import Remarkable from 'remarkable';
+import Comment from './comment';
+
+//Made module.exports for all .js files
+module.exports = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+});
