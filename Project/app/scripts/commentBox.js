9e4b3d2d51d1e04aa2f0707130f091fe602fae60
Logan VanProyen <lv33@calvin.edu>
2016-12-07 13:20:47 -0500

    moved things around

--- Project/app/scripts/commentBox.js
+++ Project/app/scripts/commentBox.js
@@ -5,38 +5,58 @@ import React from 'react';
 import $ from 'jquery';
 import CommentList from './commentList';
 import CommentForm from './commentForm';
-import { store, ActionTools } from './flux';
+import { API_URL, POLL_INTERVAL } from './global';
 
 
 //Made module.exports for all .js files
 module.exports = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentWillMount() {
-    this.unsubscribe = store.subscribe(() => {
-        this.setState({
-            data: store.getState().data
-        });
-    });
-  },
-  componentWillUnmount: function() {
-      this.unsubscribe();
-  },
-  handleCommentSubmit: function(comment) {
-    var comments = this.state.data;
-    comment.id = Date.now();
-    var newComments = comments.concat([comment]);
-    this.setState({data: newComments});
-    store.dispatch(ActionTools.addComment(comment));
-},
-render: function() {
-  return (
-    <div className="commentBox">
-    <h1>Comments</h1>
-    <CommentList data={this.state.data} />
-    <CommentForm onCommentSubmit={this.handleCommentSubmit} />
-    </div>
-  );
-}
+    getInitialState: function() {
+        return {data: []};
+    },
+    loadCommentsFromServer: function() {
+        $.ajax({
+            url: API_URL,
+            dataType: 'json',
+            cache: false,
+        })
+         .done(function(result){
+             this.setState({data: result});
+         }.bind(this))
+         .fail(function(xhr, status, errorThrown) {
+             console.error(API_URL, status, errorThrown.toString());
+         }.bind(this));
+    },
+    handleCommentSubmit: function(comment) {
+        var comments = this.state.data;
+        comment.id = Date.now();
+        var newComments = comments.concat([comment]);
+        this.setState({data: newComments});
+        $.ajax({
+            url: API_URL,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+        })
+         .done(function(result){
+             this.setState({data: result});
+         }.bind(this))
+         .fail(function(xhr, status, errorThrown) {
+             this.setState({data: comments});
+             console.error(API_URL, status, errorThrown.toString());
+         }.bind(this));
+    },
+    componentDidMount: function() {
+        this.loadCommentsFromServer();
+	console.log(POLL_INTERVAL);
+        setInterval(this.loadCommentsFromServer, POLL_INTERVAL);
+    },
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+            </div>
+        );
+    }
 });
